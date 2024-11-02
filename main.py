6c7d1b52a1693db47420381078942d6bfc6a5918
Dipanshu Tiwari <dipanshutiwari115@gmail.com>
2024-11-02 14:29:50 +0530

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -8,19 +8,42 @@ def home_page():
     return "Hello"
     # return render_template("homepage.html")
 
-@app.route('/auth/<oper>/<uname>/<passwd>')
-def authEval(oper, uname, passwd):
-    if oper == "login":
-        return render_template("homepage.html")
-    else:
-        return render_template("homepage.html")
 
-@app.route('/auth/<oper>')
-def auth(oper):
+@app.route('/auth/<oper>', defaults={'user': None, 'uname': None, 'passwd': None})
+@app.route('/auth/<oper>/<user>/<uname>/<passwd>')
+def auth(oper, user, uname, passwd):
     if oper == "login":
-        return render_template("Login")
+        if uname and passwd:
+            return render_template("login_test.html")
+        return render_template("user-auth.html")
     else:
-        return render_template("homepage.html")
+        if uname and passwd:
+            return render_template("signup_test.html")
+        return render_template("register.html")
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        roll = request.form['roll']
+        email = request.form['email']
+        permissionType = request.form['permissionType']
+        dis = request.form['dis']
+        return f"{name} {roll} {email} {dis} {permissionType}"
+    else:
+        return "nope"
+        
+
+@app.route('/dashboard/<oper>/<uname>')
+def dashboard(oper, uname):
+    # if oper is None or uname is None:
+    #     return render_template("user-auth.html")
+    if oper == "student":
+        return render_template("student.html")
+    elif oper == "admin":
+        return render_template("admin-dash.html")
+    elif oper == "faculty":
+        return render_template("approval-dash.html")
 
 @app.route('/admin-dashboard')
 def admin_dashboard():
